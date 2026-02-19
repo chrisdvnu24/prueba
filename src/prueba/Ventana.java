b1698f664d64b4d92706b942c9e4806b755d4efd
chrisdvnu24 <cvazquez_24@alu.uabcs.mx>
2026-02-19 12:41:52 -0700

    miprimeraventanaa

---
+++ src/prueba/Ventana.java
@@ -0,0 +1,21 @@
+package prueba;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame 
+{
+	public Ventana()
+	{
+		this.setVisible(true);
+		this.setSize(500, 500);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setMinimumSize(new Dimension(200, 200));
+		this.setMaximumSize(new Dimension(800, 800));
+		this.setTitle("Hola mundo");
+		this.setBackground(Color.blue);
+		this.setLocation(200, 200);
+	}
+} 
