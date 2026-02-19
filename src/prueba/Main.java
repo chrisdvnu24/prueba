1325da0cca39b232654e60af975c37d275d6d8d7
chrisdvnu24 <cvazquez_24@alu.uabcs.mx>
2026-02-19 12:13:24 -0700

    push1

---
+++ src/prueba/Main.java
@@ -0,0 +1,18 @@
+package prueba;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		JFrame ventana = new JFrame();
+		
+		ventana.setVisible(true);
+		ventana.setSize(500, 500);
+		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ventana.setLocationRelativeTo(null);
+	}
+
+}
